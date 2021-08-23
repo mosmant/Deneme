2a537a14fc4ff270b99f39dbb473e73c69c0f1cf
mosmant <mottnr@gmail.com>
2021-08-23 16:50:45 +0300

    23.08.2021-1

---
+++ Denemeler/src/Deneme_03.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Deneme_03 {
+
+	public static void main(String[] args) {
+		// Interview Question Kullanicidan 50'�'den kucuk bir sayi isteyin ve girilen
+		// sayinin faktoryelini bulun. (5!=5*4*3*2*1)
+		
+		Scanner scan =new Scanner (System.in);
+		System.out.print("lütfen 10'dan küçükk 1 tam sayı giriniz:");
+		int num = scan.nextInt();
+		int faktoryel=1;
+		
+		System.out.print(num+ "!=");
+		for (int i = num; i>=2; i--) {
+			faktoryel= faktoryel*i;
+			System.out.print(i);
+			if (i!=num+1) {
+				System.out.print("*");
+			}
+		}System.out.println("1="+faktoryel);
+		
+
+	}
+
+}
